ac4f0fd498c0dcddfc2e0f2fe21ac1d98cd044ea
Antom91 <anton.marusenko@onix-systems.com>
2022-03-15 11:05:40 +0200

    Fix playlist

--- run.py
+++ run.py
@@ -1,20 +1,28 @@
 import os
 from subprocess import PIPE, run
 
+# videos_list = run('youtube-dl --get-id https://www.youtube.com/playlist?list=PLSV_wtTonUmR-s0obXZn2OsNeota6TMxt -i', stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).stdout.strip().split('\n')
+# my_string = ','.join(videos_list)
+# print(my_string)
+# exit(1)
+
 YOUTUBE_URL='https://youtu.be/'
 VIDEO_LINK_QUALITY = 'best'
 VIDEO_FILE_EXTENSION = '.mp4'
 TMP_DIR = os.path.join('/', 'tmp')
 
 try:
-    # PLAYLIST = os.environ['PLAYLIST']
-    PLAYLIST = 'PLSV_wtTonUmR-s0obXZn2OsNeota6TMxt'
+    PLAYLIST = os.environ['PLAYLIST']
 except:
     print("Playlist not found")
+    exit(1)
 
-videos_list = run('youtube-dl --get-id https://www.youtube.com/playlist?list={} -i'.format(PLAYLIST), stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).stdout.strip().split('\n')
-
-for i in videos_list:
+print()
+print()
+print(PLAYLIST)
+print()
+print()
+for i in PLAYLIST:
     # os.system('')
     file_input = os.path.join(TMP_DIR, i+VIDEO_FILE_EXTENSION)
     file_output = os.path.join(TMP_DIR, i+'_output'+VIDEO_FILE_EXTENSION)
@@ -24,7 +32,7 @@ for i in videos_list:
     os.system('aria2c -k 1M -s 128 -x 128 -o "..{output}" "{url}"'.format(url=file_video_link, output=file_input))
     os.system('ffmpeg -i {file_input} -crf 15 -vf scale=1920x1080:flags=lanczos -aspect 16:9 {file_output}'.format(file_input=file_input, file_output=file_output))
 
-    cmd = os.system('youtube-upload --file="{file_output}" --title="{video_title}" --category="22" --privacyStatus="private"'.format(file_output=file_input, video_title=video_title))
+    cmd = os.system('youtube-upload --file="{file_output}" --title="{video_title}" --description="{video_title}" --category="22" --privacyStatus="private"'.format(file_output=file_input, video_title=video_title))
 
     os.system('rm -rf {} {}'.format(file_input,file_output))
     exit_code = os.WEXITSTATUS(cmd)
