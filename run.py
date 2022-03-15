092e837e40433732bf781d9f1db11e6e47440066
Antom91 <anton.marusenko@onix-systems.com>
2022-03-15 13:26:27 +0200

    Fix titles

--- run.py
+++ run.py
@@ -17,11 +17,11 @@ except:
     print("Playlist not found")
     exit(1)
 
-print()
-print()
+print("===============================================")
+print("===============================================")
 print(PLAYLIST)
-print()
-print()
+print("===============================================")
+print("===============================================")
 
 PLAYLIST = PLAYLIST.split(',')
 for i in PLAYLIST:
@@ -31,9 +31,13 @@ for i in PLAYLIST:
     video_title = run('youtube-dl --skip-download --get-title --no-warnings {youtube}{yt_video_path}'.format(yt_video_path=i, youtube=YOUTUBE_URL), stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).stdout.strip()
     file_video_link = run('youtube-dl -f {video_link_quality} -g {youtube}{yt_video_path}'.format(video_link_quality=VIDEO_LINK_QUALITY, yt_video_path=i, youtube=YOUTUBE_URL), stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True).stdout
 
+    print("Downloading video from YouTube")
     os.system('aria2c -k 1M -s 128 -x 128 -o "..{output}" "{url}"'.format(url=file_video_link, output=file_input))
+
+    print("Converting video from YouTube")
     os.system('ffmpeg -i {file_input} -crf 15 -vf scale=1920x1080:flags=lanczos -aspect 16:9 {file_output}'.format(file_input=file_input, file_output=file_output))
 
+    print("Uploading to YouTube")
     cmd = os.system('youtube-upload --file="{file_output}" --title="{video_title}" --description="{video_title}" --category="22" --privacyStatus="private"'.format(file_output=file_input, video_title=video_title))
 
     os.system('rm -rf {} {}'.format(file_input,file_output))
